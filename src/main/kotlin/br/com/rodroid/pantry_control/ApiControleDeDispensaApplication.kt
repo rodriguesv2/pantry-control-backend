458110fb640ed401a6f8a88621d927dd1e716586
Rubens Rodrigues <rubensrodriguesv2@gmail.com>
2026-01-21 17:56:23 -0300

    Big Bang

---
+++ src/main/kotlin/br/com/rodroid/pantry_control/ApiControleDeDispensaApplication.kt
@@ -0,0 +1,11 @@
+package br.com.rodroid.pantry_control
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiControleDeDispensaApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiControleDeDispensaApplication>(*args)
+}
